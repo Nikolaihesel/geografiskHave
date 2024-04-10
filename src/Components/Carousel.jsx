11c425de69150308972ab1a6a098606b8a87d7ac
Maria Nordahl Dimon <momeria.dimon@gmail.com>
2024-04-10 10:21:54 +0200

    MainView Intro Component
    Intro Component.
    Start on Carousel Component

---
+++ src/Components/Carousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CarouselCard from './CarouselCard'
+
+function Carousel() {
+  return (
+    <div>
+        <h3>Hvilken fortælling vil du høre?</h3>
+        <CarouselCard StoryImg={} StoryTitle={"Vinterens Silhuetter"}/>
+    </div>
+  )
+}
+
+export default Carousel
