23bd51238367934c69f15c4f8f61d37312c26ac4
Nikolaihesel <Nikolaihesel@icloud.com>
2024-04-10 10:38:20 +0200

    mapping carousel cards
    Co-Authored-By: Maria Nordahl Dimon <83703270+M-Dimon@users.noreply.github.com>
    Co-Authored-By: Reitc <166492160+Reitc@users.noreply.github.com>
    Co-Authored-By: Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import CarouselCard from './CarouselCard'
+import React from 'react';
+import WintherImg from '../image/vinter-silhuet.jpg';
+import AsianFlower from '../image/asiatisk-blomst.png';
+
+//component
+import CarouselCard from '../Components/CarouselCard';
+const carouselData = {
+	stories: [
+		{
+			id: 1,
+			title: 'Vinter Silhuetter',
+			img: WintherImg,
+		},
+		{
+			id: 2,
+			title: 'Geografiskhaves historie',
+			img: AsianFlower,
+		},
+		{
+			id: 3,
+			title: 'Asiens eventyrlige planer',
+			img: AsianFlower,
+		},
+	],
+};
 
 function Carousel() {
-  return (
-    <div>
-        <h3>Hvilken fortælling vil du høre?</h3>
-        <CarouselCard StoryImg={} StoryTitle={"Vinterens Silhuetter"}/>
-    </div>
-  )
+	return (
+		<div>
+			{carouselData &&
+				carouselData.stories.map((card) => (
+					<CarouselCard
+						key={card.id}
+						StoryImg={card.img}
+						StoryTitle={card.title}
+					/>
+				))}
+		</div>
+	);
 }
 
-export default Carousel
+export default Carousel;
