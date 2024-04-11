10ac91aee1bf63ac3363e5adc76d424eb68321c0
Nikolaihesel <Nikolaihesel@icloud.com>
2024-04-11 12:30:25 +0200

    nav open/close

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 // CSS modules
-import Style from "../assets/styles/components/modules/nav.module.scss";
+import Style from '../assets/styles/components/modules/nav.module.scss';
 
 function Nav() {
-  return (
-    <nav className={Style.nav}>
-        <h1>Geografisk Have</h1>
-        <div className='ticket-icon'>1</div>
-        <div className='search-icon'>2</div>
-        <div className='burger-menu'>3</div>
-    </nav>
-  )
+	const [nav, setNav] = useState(false);
+	return (
+		<nav className={Style.nav}>
+			<div className={Style.navContainer}>
+				<h1>Geografisk Have</h1>
+				<div className='ticket-icon'>1</div>
+				<div className='search-icon'>2</div>
+				<div
+					onClick={() => setNav(!nav)}
+					className='burger-menu'>
+					<div className={Style.burger}></div>
+				</div>
+			</div>
+			{nav && (
+				<div className={Style.navMenu}>
+					<ul>
+						<li>Home</li>
+						<li>About</li>
+						<li>Services</li>
+						<li>Contact</li>
+					</ul>
+				</div>
+			)}
+		</nav>
+	);
 }
 
-export default Nav
+export default Nav;
