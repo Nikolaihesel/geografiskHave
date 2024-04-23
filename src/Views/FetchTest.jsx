02ee7497257dd15d0d2326b904b36c8eafaaef3d
ebuchhansen <ebha38482@edu.ucl.dk>
2024-04-23 16:05:32 +0200

    update fetch and get test files
    fetch file works mostly, except img
    get test is started, but needs some more
    
    Co-Authored-By: Maria Nordahl Dimon <83703270+M-Dimon@users.noreply.github.com>

--- src/Views/FetchTest.jsx
+++ src/Views/FetchTest.jsx
@@ -1,59 +1,38 @@
-import React from 'react';
-import { getFirestore } from 'firebase/firestore'
+import React, { useState } from 'react';
 import { collection, addDoc } from "firebase/firestore";
-import firebaseSetup from '../firebaseConfig';
-import { initializeApp } from 'firebase/app';
+import { db } from '../config/firebase';
 
 //Make into POST instead - addDoc function
 
-function PostTest({title, imgFile, imgFileAlt }) {
+function PostTest() {
 
-	const firebaseConfig = {
-		apiKey: "AIzaSyCdoyP8-Yvp_dODgygoa0wCqqIMChZ3yxI",
-		authDomain: "geografisk-have-eksamen.firebaseapp.com",
-		projectId: "geografisk-have-eksamen",
-		storageBucket: "geografisk-have-eksamen.appspot.com",
-		messagingSenderId: "655966352285",
-		appId: "1:655966352285:web:5af953758470ba6c8c3574"
+	const [title, setTitle] = useState('');
+
+	// need fixing perhaps  //const [img, setImg] = useState('');
+
+	const [imgAlt, setImgAlt] = useState('');
+
+	const addToDb = async () => {
+		try {
+			const data = {
+			title: title,
+			// img: img,
+			imgAlt: imgAlt,
+			};
+
+			const docRef = await addDoc(collection(db, 'stories'), data);
+			console.log('Document added with ID:', docRef.id);
+		} catch (error) {
+			console.error('Error adding document:', error);
+		}
 	};
-
-	const app = initializeApp(firebaseConfig);
-
-	const db = getFirestore(app);
-//So we can get it to send data, however it does not take the values in the input fields, which is what we are trying to get it to do.
-	function getInputValues(className) {
-	const inputElement = document.querySelector(`.${className}`);
-	return inputElement ? inputElement.value : null;
-	}	
-
-	const handleAddDoc = async () => {
-	try {
-		const titleValue = getInputValues('title'); // Replace with actual class name
-		const imgValue = getInputValues('imgFile'); // Replace with actual class name
-		const imgAltValue = getInputValues('imgFileAlt'); // Replace with actual class name
-
-		const data = {
-		title: titleValue,
-		img: imgValue,
-		imgAlt: imgAltValue,
-		};
-
-		// Add the data to the Firestore collection
-		const docRef = await addDoc(collection(db, 'stories'), data);
-		console.log('Document added with ID:', docRef.id);
-	} catch (error) {
-		console.error('Error adding document:', error);
-	}
-	};
-
-
 
 	return (
 		<div className="createStory">
-			<input className={title} type="text" placeholder="Title" />
-			<input className={imgFile} type="file" accept="image/*" />
-			<input className={imgFileAlt} type="text" placeholder="Image Alt Text" />
-			<button onClick={handleAddDoc}>Add to Firestore</button>
+			<input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
+			{/* <input type="file" accept="image/*" onChange={(e) => setImg(e.target.value)} /> */}
+			<input type="text" placeholder="Image Alt Text" onChange={(e) => setImgAlt(e.target.value)} />
+			<button onClick={addToDb}>Add to Firestore</button>
 		</div>
 	);
 }
--- src/Views/GetTest.jsx
+++ src/Views/GetTest.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { collection, getDoc } from "firebase/firestore";
+import { db } from '../config/firebase';
 
 function GetTest() {
-	return <div>GetTest</div>;
+	
+	const [title, setTitle] = useState('');
+
+	// need fixing perhaps  //const [img, setImg] = useState('');
+
+	const [imgAlt, setImgAlt] = useState('');
+
+	const getFromDb = async () => {
+		try {
+			const data = {
+				title: title,
+				// img: img,
+				imgAlt: imgAlt,
+			};
+
+			const docRef = await getDoc(collection(db, 'stories'), data);
+			console.log('Document added with ID:', docRef.id);
+
+		} catch (error) {
+			console.error('Error fetching document:', error);
+		}
+	};
+	
+	return (
+		<div className='updateStory'>
+			
+		</div>
+	);
 }
 
 export default GetTest;
