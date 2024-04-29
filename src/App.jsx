c3e3ae3161c2c43c2fb7bb85b7e38fd3601ab072
Nikolaihesel <Nikolaihesel@icloud.com>
2024-04-29 11:40:55 +0200

    Deleted unused views

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import MainView from './Views/MainView';
 import Nav from './Components/Nav';
 import MapView from './Views/MapView';
-import FetchTest from './Views/FetchTest';
-
-import AuthTest from './Views/AuthTest';
 import Admin from './Views/admin/Admin';
 import AddStory from './Views/admin/childComponents/AddStory';
 import UpdateStory from './Views/admin/childComponents/UpdateStory';
@@ -27,15 +24,7 @@ function App() {
 						path='map'
 						element={<MapView />}
 					/>
-					<Route
-						path='fetchtest'
-						element={<FetchTest />}
-					/>
 
-					<Route
-						path='authTest'
-						element={<AuthTest />}
-					/>
 					<Route
 						path='/'
 						element={<Admin />}>
--- src/Views/AuthTest.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AuthTest() {
-	return <div>AuthTest</div>;
-}
-
-export default AuthTest;
