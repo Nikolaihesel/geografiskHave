6700e3b96ec21eb1db85423ad462e0524b961d32
Nikolaihesel <Nikolaihesel@icloud.com>
2024-04-30 16:57:29 +0200

    added alias at root src folder

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
-
-//TODO - Add alias to vite.config.js
-// import { defineConfig } from 'vite';
-// import reactRefresh from '@vitejs/plugin-react-refresh';
-// import path from 'path';
-
-// export default defineConfig({
-//   plugins: [reactRefresh()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, 'src'),
-
-//     },
-//   },
-// });
