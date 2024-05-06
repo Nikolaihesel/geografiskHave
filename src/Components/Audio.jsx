4fb3eeff32bce1f454048207414fe62c78860932
Maria Dimon <momeria.dimon@gmail.com>
2024-05-06 13:39:03 +0200

    Audio player

--- src/Components/Audio.jsx
+++ src/Components/Audio.jsx
@@ -1,10 +1,49 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import audioFile from '../assets/audio/dally.mp3'; 
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+import { getDoc, collection } from 'firebase/firestore';
+import { db } from '../config/firebase';
 
 import Style from '../assets/styles/components/modules/draggable.module.scss';
 
 function Audio() {
+ 
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [file, setFile] = useState(null);
+  const [audio, setAudio] = useState(null);
+  const [markerText, setMarkerText] = useState('');
+  const storage = getStorage();
+  const [markerLocations, setMarkerLocations] = useState([]);
+  
   const audioRef = useRef(null);
+  
+  const onMapMount = async (e) => {
+    e.preventDefault();
+    
+    async () => { 
+    const downloadURL = await getDownloadURL(fetchTask.snapshot.ref);
+
+    const docRef =  await getDoc(collection(db, 'stories'), {
+      title: title,
+      description: description,
+      image: downloadURL,
+      audio: audio,
+      markerText: markerText,
+      markerLocations: markerLocations,
+    })};
+  }
+
+  useEffect(() => {
+    onMapMount();
+  }, []);
+
+ 
   return (
     <div className={Style.draggableWrapper}>
         <audio controls ref={audioRef}>
