67581c92df5305338c4a39763a6d89e5215cc18d
Maria Nordahl Dimon <momeria.dimon@gmail.com>
2024-06-17 12:03:36 +0200

    PWA setup
    Files for PWA setup, created and tested.
    Lighthouse = PWA downloadable

--- src/main.jsx
+++ src/main.jsx
@@ -10,3 +10,54 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		</UserProvider>
 	</React.StrictMode>
 );
+
+// Service Worker Registrering
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(error => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
+
+// Tilføjelse til Hjemskærm Prompt
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  // Update UI notify the user they can add to home screen
+  showAddToHomeScreen();
+});
+
+function showAddToHomeScreen() {
+  // Show the add to home screen button
+  const addToHomeScreenButton = document.querySelector('.add-to-home-screen-button');
+  addToHomeScreenButton.style.display = 'block';
+
+  addToHomeScreenButton.addEventListener('click', addToHomeScreen);
+}
+
+function addToHomeScreen() {
+  // Hide the add to home screen button
+  const addToHomeScreenButton = document.querySelector('.add-to-home-screen-button');
+  addToHomeScreenButton.style.display = 'none';
+
+  // Show the prompt
+  deferredPrompt.prompt();
+
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
+    }
+    deferredPrompt = null;
+  });
+}
